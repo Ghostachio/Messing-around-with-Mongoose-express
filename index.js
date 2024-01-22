0b1a8b6aa6a50cb4e0a2a3609a0554ab6d8a5e29
Ghostachio <mansour.hamza2.0@gmail.com>
2024-01-22 23:44:54 +0100

    added create functionality

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const seeds = require('./seeds')
 
 
 
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended : true}))
 
 
 
@@ -17,6 +18,17 @@ app.get('/products', async (req,res) =>{
   const products = await Product.find({})
   console.log(products)
   res.render('products/index', {products})
+})
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new')
+})
+
+app.post('/products', async (req, res)=> {
+  const newProduct =  new Product(req.body);
+  await newProduct.save();
+  console.log(newProduct)
+  res.redirect('products')
 })
 
 
@@ -28,9 +40,10 @@ app.get('/products/:id', async (req, res) => {
 })
 
 
-app.get('/products/new', (req, res) => {
-  res.render('products/new')
-})
+
+
+
+
 
 app.listen('3000',()=>{
   console.log('listening to port 3000')
