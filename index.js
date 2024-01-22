bd133025bb98872770a478be0d43e9f5849a7fa4
Ghostachio <mansour.hamza2.0@gmail.com>
2024-01-22 22:52:40 +0100

    fezfe

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-// setting up express
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const Product = require('./models/product');
 const seeds = require('./seeds')
-//
 
 
 
-app.set('views', path.join(__dirname, 'views'));
+
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 
-// index page to list all products
+
 app.get('/products', async (req,res) =>{
   const products = await Product.find({})
   console.log(products)
   res.render('products/index', {products})
 })
 
-// page to show details about a spesific product
+
 app.get('/products/:id', async (req, res) => {
   const {id} = req.params;
   const product = await Product.findById(id)
   console.log(product)
   res.render('products/show', {product})
+})
+
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new')
 })
 
 app.listen('3000',()=>{
