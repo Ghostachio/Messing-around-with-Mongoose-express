0cc66b2725d4bbaff166ce7af45128ec51b5ef7a
Ghostachio <mansour.hamza2.0@gmail.com>
2024-01-23 23:52:49 +0100

    full CRUD implemented

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-
+//requiring Express
 const express = require('express');
 const app = express();
 const path = require('path');
 const Product = require('./models/product');
 const seeds = require('./seeds')
+const methodOverride = require('method-override');
 
 
 
-
+// setting views path, engine, middleware and url encoder
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended : true}))
+app.use(methodOverride('_method'))
+
+categories = ["fruit", "vegetables", "dairy"]
 
 
-
+// index page showing all products
 app.get('/products', async (req,res) =>{
   const products = await Product.find({})
   console.log(products)
   res.render('products/index', {products})
 })
-
+//form to create new product
 app.get('/products/new', (req, res) => {
-  res.render('products/new')
+  res.render('products/new', {categories})
 })
-
+// getting daat from form and going back to index
 app.post('/products', async (req, res)=> {
   const newProduct =  new Product(req.body);
   await newProduct.save();
@@ -31,7 +35,28 @@ app.post('/products', async (req, res)=> {
   res.redirect('products')
 })
 
+// form to edit one product
+app.get('/products/:id/edit', async(req, res) => {
+  const {id} = req.params;
+  const product = await Product.findById(id)
+  res.render("products/edit", {product, categories})
+})
+// submiting the edited product
+app.put('/products/:id', async (req, res)=> {
+  const {id} = req.params;
+  const updateProduct = await Product.findByIdAndUpdate(id, req.body, {runValidators: true})
+  console.log(updateProduct)
+  res.redirect(`/products/${updateProduct._id}`)
+})
 
+// deleting product 
+app.delete('/products/:id', async (req, res) => {
+  const {id} = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect('/products')
+})
+
+// showing details of one selected product  
 app.get('/products/:id', async (req, res) => {
   const {id} = req.params;
   const product = await Product.findById(id)
